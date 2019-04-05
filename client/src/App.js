39481d35425c75a49600092723919be686f53757
Ashish <akjha992@gmail.com>
2019-04-05 13:23:11 +0530

    loaded app

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,127 @@
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  // Initialize state
-  state = { passwords: [] }
-
-  // Fetch passwords after first mount
+import React from "react";
+import ReactHLS from 'react-hls';
+import desktop from './desktop.css';
+import phone from './phone.css';
+import MQ from 'react-responsive';
+import axios from 'axios';
+const baseUrl = {
+  hin: "https://cors-anywhere.herokuapp.com/https://live12.akt.hotstar-cdn.net/hls/live/2003697/ipl2019/hin/fulldvrm0",
+  eng: "https://cors-anywhere.herokuapp.com/https://live11.akt.hotstar-cdn.net/hls/live/2003689/ipl2019/eng/fulldvrm0"
+};
+const breakpoints = {
+  desktop: '(min-width: 1025px)',
+  tablet: '(min-width: 768px) and (max-width: 1024px)',
+  phone: '(max-width: 767px)',
+ };
+ const d = new Date();
+const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+let month = monthNames[d.getMonth()].toLowerCase();
+let date = d.getDate();
+let matchNo =1;
+if (d.getHours() >=20){
+    matchNo = 2;
+}
+if (d.getHours() <1){
+    date--;
+    matchNo = 2;
+}
+if (date<10){
+    date='0'+date;
+}
+date = '04';
+month = 'april'
+matchNo = 1;
+class App extends React.Component {
+  constructor(){
+    super();
+    this.load = this.load.bind(this);
+    this.quality = React.createRef();
+    this.language = React.createRef();
+    this.getItems = this.getItems.bind(this);
+    this.state = {
+      url: '',
+      updated: 0
+    }
+  }
   componentDidMount() {
-    this.getPasswords();
+    this.timer = setInterval(()=> this.getItems(), 5000);
   }
-
-  getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+  
+  componentWillUnmount() {
+    this.timer = null; // here...
   }
-
+  getItems() {
+    axios.get(this.state.url)
+  .then((response) => {
+      axios.post('/save',{
+        response: response.data
+      })
+      .then( () => {
+        console.log('saved');
+        if(this.state.prevUrl !== this.state.url)
+        {
+          console.log('rerendered');
+          this.setState((state) => ({
+              prevUrl: state.url
+          }))
+        }
+    });
+  });
+  }
+  load(e){
+      e.preventDefault();
+    this.setState((state) => ({
+        prevUrl: state.url,
+        url: baseUrl[this.language.current.value]+matchNo + date + month + "2019/master_"+this.quality.current.value+".m3u8",
+    }));
+  }
   render() {
-    const { passwords } = this.state;
-
     return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
+      <div>
+        <MQ query={breakpoints.desktop}>
+          <div className='sideBar'>
+          <label className='label'>Select Language</label>
+                <select className={'select-css'} name="language" ref={this.language} defaultValue='eng'>
+                  <option value='eng'>English</option>
+                  <option value='hin'>Hindi</option>
+                </select>
+              <label className={'label'}>Select Quality</label>
+              <select className={'select-css'} name="quality" ref={this.quality} defaultValue='1'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+              </select>
+              <button className={'button'} onClick={this.load}>LOAD</button>
           </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
+          <div className={'mainBody'}>
+            <ReactHLS width={800} height={600} url={'/watch.m3u8'} autoplay/>
           </div>
-        )}
+        </MQ>
+        <MQ query={breakpoints.phone}>
+          <div className={phone.mainBody}>
+            <ReactHLS width={window.innerWidth} height={200} url={this.state.url} autoplay/>
+          </div>
+          <div className={phone.sideBar}>
+          <label className={phone.label}>Select Language</label>
+                <select className={desktop['select-css']} name="language" ref={this.language} defaultValue='eng'>
+                  <option value='eng'>English</option>
+                  <option value='hin'>Hindi</option>
+                </select>
+              <label className={phone.label}>Select Quality</label>
+              <select className={phone['select-css']} name="quality" ref={this.quality} defaultValue='1'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+              </select>
+              <button className={phone.button} onClick={this.load}>LOAD</button>
+          </div>
+        </MQ>
       </div>
     );
   }
 }
-
 export default App;
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const generatePassword = require('password-generator');
+const fs = require('fs');
+const request = require('request');
 
 const app = express();
-
+app.use( bodyParser.json({limit: '10mb', extended: true}) ); 
+app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -20,6 +24,43 @@ app.get('/api/passwords', (req, res) => {
   res.json(passwords);
 
   console.log(`Sent ${count} passwords`);
+});
+app.post('/save', function(req, res) {
+  const content =   req.body.response;
+  const result = content.replace(/https:\/\//g, "/play?url=https://");
+  fs.writeFile('./server/data.m3u8', result , function(err) {
+    if (err) {
+       res.status(500).jsonp({ error: 'Failed to write file' });
+    }
+    res.send("File write success");
+  });
+});
+app.get('/watch.m3u8', (req, res) => {
+  res.setHeader("content-type", "some/type");
+  fs.readFile("./server/data.m3u8", 'utf8', function(err, data) {  
+    if (err) res.send(err);
+    else {
+      fs.createReadStream("./server/data.m3u8").pipe(res);
+    }
+});
+});
+app.get('/play', function(req,res) {
+  const tag = req.query.url;
+  const newurl = tag;
+  console.log(newurl);
+  var newReq = request(newurl, function(error) {
+    if (error) {
+        logError(error);
+    }
+  });
+  req.pipe(newReq)
+      .on('response',
+        function (response) {
+          res.writeHead(response.statusCode, {
+            ...response.headers,
+          });
+        }
+      ).pipe(res);
 });
 
 // The "catchall" handler: for any request that doesn't
