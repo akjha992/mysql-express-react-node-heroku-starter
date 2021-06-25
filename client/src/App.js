45aa876ae2420009ed20b60466071b1ff09892f3
Ashish Jha <akjha992@gmail.com>
2021-06-25 11:59:55 +0530

    making template

--- client/src/App.js
+++ client/src/App.js
@@ -1,127 +1,21 @@
 import React from "react";
-import ReactHLS from 'react-hls';
-import MQ from 'react-responsive';
-import axios from 'axios';
-const baseUrl = {
-  hin: "https://cors-anywhere.herokuapp.com/https://live12.akt.hotstar-cdn.net/hls/live/2003697/ipl2019/hin/fulldvrm0",
-  eng: "https://cors-anywhere.herokuapp.com/https://live11.akt.hotstar-cdn.net/hls/live/2003689/ipl2019/eng/fulldvrm0"
-};
-const breakpoints = {
-  desktop: '(min-width: 1025px)',
-  tablet: '(min-width: 768px) and (max-width: 1024px)',
-  phone: '(max-width: 767px)',
- };
- const d = new Date();
-const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-let month = monthNames[d.getMonth()].toLowerCase();
-let date = d.getDate();
-if (d.getHours() <1){
-    date--;
-}
-if (date<10){
-    date='0'+date;
-}
+import axios from "axios";
 class App extends React.Component {
   constructor(){
     super();
-    this.load = this.load.bind(this);
-    this.quality = React.createRef();
-    this.language = React.createRef();
-    this.matchNo = React.createRef();
-    this.getItems = this.getItems.bind(this);
-    this.state = {
-      url: '',
-      updated: 0
-    }
+    this.state={users: []};
   }
-  componentDidMount() {
-    console.log(window.innerWidth, window.innerWidth*600/800);
-    this.timer = setInterval(()=> this.getItems(), 5000);
-  }
-  
-  componentWillUnmount() {
-    this.timer = null; // here...
-  }
-  getItems() {
-    axios.get(this.state.url)
-  .then((response) => {
-      axios.post('/save',{
-        response: response.data
-      })
-      .then( () => {
-        console.log('saved');
-        if(this.state.prevUrl !== this.state.url)
-        {
-          console.log('rerendered');
-          this.setState((state) => ({
-              prevUrl: state.url
-          }))
-        }
+  componentDidMount(){
+    axios.get('/users').then(resp=>{
+      console.log(resp.data);
+      this.setState({users: resp.data});
     });
-  });
-  }
-  load(e){
-      e.preventDefault();
-    this.setState((state) => ({
-        prevUrl: state.url,
-        url: baseUrl[this.language.current.value]+this.matchNo.current.value + date + month + "2019/master_"+this.quality.current.value+".m3u8",
-    }));
   }
   render() {
     return (
       <div>
-        <MQ query={breakpoints.desktop}>
-          <div className='sideBar'>
-          <label className='label'>Select Language</label>
-                <select className={'select-css'} name="language" ref={this.language} defaultValue='eng'>
-                  <option value='eng'>English</option>
-                  <option value='hin'>Hindi</option>
-                </select>
-                <label className='label'>Select Match</label>
-                <select className={'select-css'} name="matchNo" ref={this.matchNo} defaultValue='1'>
-                  <option value='1'>First Match of the day</option>
-                  <option value='2'>Second Match of the day</option>
-                </select>
-              <label className={'label'}>Select Quality</label>
-              <select className={'select-css'} name="quality" ref={this.quality} defaultValue='1'>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-              </select>
-              <button className={'button'} onClick={this.load}>LOAD</button>
-          </div>
-          <div className={'mainBody'}>
-            <ReactHLS width={800} height={600} url={'/watch.m3u8'} autoplay/>
-          </div>
-        </MQ>
-        <MQ query={breakpoints.phone}>
-        <div className={'player-phone'}>
-        <ReactHLS width={window.innerWidth} height={(window.innerWidth*600)/800} url={'/watch.m3u8'} autoplay/>
-          </div>
-          <div className='selector-phone'>
-          <label className='label'>Select Language</label>
-                <select className={'select-css'} name="language" ref={this.language} defaultValue='eng'>
-                  <option value='eng'>English</option>
-                  <option value='hin'>Hindi</option>
-                </select>
-                <label className='label'>Select Match</label>
-                <select className={'select-css'} name="matchNo" ref={this.matchNo} defaultValue='1'>
-                  <option value='1'>First Match of the day</option>
-                  <option value='2'>Second Match of the day</option>
-                </select>
-              <label className={'label'}>Select Quality</label>
-              <select className={'select-css'} name="quality" ref={this.quality} defaultValue='1'>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-              </select>
-              <button className={'button'} onClick={this.load}>LOAD</button>
-          </div>
-        </MQ>
+        <h1>User List</h1>
+        {this.state.users.map(user=>(<h2>{user.userName}</h2>))}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,67 +1,38 @@
 const express = require('express');
+const mysql = require('mysql');
 const path = require('path');
 const bodyParser = require('body-parser');
-const generatePassword = require('password-generator');
-const fs = require('fs');
-const request = require('request');
+const axios = require('axios');
 
 const app = express();
-app.use( bodyParser.json({limit: '10mb', extended: true}) ); 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use( bodyParser.json({limit: '10mb', extended: true}) ); 
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// create connection
+const con = mysql.createConnection({
+  host: "remotemysql.com",
+  user: "r3AHiWjIhC",
+  password: "NC35oj5Kyu",
+  database: "r3AHiWjIhC"
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-app.post('/save', function(req, res) {
-  const content =   req.body.response;
-  const result = content.replace(/https:\/\//g, "/play?url=https://");
-  fs.writeFile('./server/data.m3u8', result , function(err) {
-    if (err) {
-       res.status(500).jsonp({ error: 'Failed to write file' });
-    }
-    res.send("File write success");
+app.get('/users', (req, res) => {
+  // get all usernames
+  con.query('select * from User', function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+    res.json(result);
   });
 });
-app.get('/watch.m3u8', (req, res) => {
-  res.setHeader("content-type", "some/type");
-  fs.readFile("./server/data.m3u8", 'utf8', function(err, data) {  
-    if (err) res.send(err);
-    else {
-      fs.createReadStream("./server/data.m3u8").pipe(res);
-    }
-});
-});
-app.get('/play', function(req,res) {
-  const tag = req.query.url;
-  const newurl = tag;
-  console.log(newurl);
-  var newReq = request(newurl, function(error) {
-    if (error) {
-        logError(error);
-    }
-  });
-  req.pipe(newReq)
-      .on('response',
-        function (response) {
-          res.writeHead(response.statusCode, {
-            ...response.headers,
-          });
-        }
-      ).pipe(res);
-});
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -72,4 +43,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Demo App ${port}`);
